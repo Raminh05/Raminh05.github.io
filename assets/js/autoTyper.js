c05bb5fbe33a0c2e3d736135ec2b6a6708b6511c
Raminh05 <77513059+Raminh05@users.noreply.github.com>
2022-08-29 17:24:12 -0400

    Hide horizontal scroll bar on all pages

--- assets/js/autoTyper.js
+++ assets/js/autoTyper.js
@@ -1,3 +1,4 @@
+// Object initialization to start the auto-typer
 var typed = new Typed(".auto-type", {
     strings: ["And coding is what I do!"],
     typeSpeed: 65
--- assets/js/hideNavBar.js
+++ assets/js/hideNavBar.js
@@ -1,14 +1,23 @@
+/* Simple script to hide the navbar upon scroll */
+
+// Previous scroll position
 let prevScrollpos = window.scrollY;
 
+// onscroll event
 window.onscroll = function() {
+  // Fetches current scroll position
   let currentScrollPos = window.scrollY;
-
+    
+  // If scroll position is at the top or if the user scrolled back up, show the navbar
   if (prevScrollpos > currentScrollPos || currentScrollPos <= 20) {
     document.getElementById("mainNavbar").style.top = "0";
   }
+
+  // Hide the navbar if scrolling down
   else {
     document.getElementById("mainNavbar").style.top = "-100px";
   }
-
+  
+  // Updates current scroll position
   prevScrollpos = currentScrollPos;
 }
